0974bbd89330b391cfe4e5ffa82e244fecdb090b
sndhuArpan <37368719+sndhuArpan@users.noreply.github.com>
2018-04-17 18:19:28 +0530

    Add files via upload

---
+++ ShortTermCalls/Domains/ShareCal.py
@@ -0,0 +1,12 @@
+'''
+Created on Apr 13, 2018
+
+@author: C-Arpanjeet.Sandhu
+'''
+
+
+class ShareCall:
+    chatId = ""
+    text = ""
+    date = ""
+    
---
+++ ShortTermCalls/Domains/TextDecode.py
@@ -0,0 +1,12 @@
+'''
+Created on Apr 17, 2018
+
+@author: C-Arpanjeet.Sandhu
+'''
+class TextDecode:
+    typeMethod = ""
+    shareName = ""
+    noOfShare = ""
+    price = ""
+    stopLoss = ""
+    targetPrice = ""
